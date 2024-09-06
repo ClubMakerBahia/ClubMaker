e63071cf2deaa7ab82ab6c59de4e505f993e4e78
IkrlegidO <Ikermardi2010huawei@gmail.com>
2024-09-06 14:24:12 +0200

    Added Proyects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [react(), sentry(), spotlightjs()]
 });
